309c39c4ad75c7dab25a7875616082521133642c
igord <igordiasth@gmail.com>
2024-08-28 12:16:16 -0400

    update `script.js`

--- static/script.js
+++ static/script.js
@@ -1,21 +1,21 @@
-document.getElementById("shorten-form").addEventListener("submit", async function(event) {
-    event.preventDefault();
-    const urlInput = document.getElementById("url-input").value;
-
-    const response = await fetch("/url", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ target_url: urlInput }),
-    });
-
-    if (response.ok) {
-        const result = await response.json();
-        document.getElementById("shortened-url").href = result.url;
-        document.getElementById("shortened-url").textContent = result.url;
-        document.getElementById("result").style.display = "block";
-    } else {
-        alert("Failed to shorten the URL.");
-    }
-});
+//document.getElementById("shorten-form").addEventListener("submit", async function(event) {
+//    event.preventDefault();
+//    const urlInput = document.getElementById("url-input").value;
+//
+//    const response = await fetch("/url", {
+//        method: "POST",
+//        headers: {
+//            "Content-Type": "application/json",
+//        },
+//        body: JSON.stringify({ target_url: urlInput }),
+//    });
+//
+//    if (response.ok) {
+//        const result = await response.json();
+//        document.getElementById("shortened-url").href = result.url;
+//        document.getElementById("shortened-url").textContent = result.url;
+//        document.getElementById("result").style.display = "block";
+//    } else {
+//        alert("Failed to shorten the URL.");
+//    }
+//});
