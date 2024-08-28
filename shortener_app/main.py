40a266faa7af914879931279716dc5f906fc7171
igord <igordiasth@gmail.com>
2024-08-28 09:29:12 -0400

    add route that forwards to the target URL `main.py`

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,6 +1,7 @@
 import secrets
 import validators
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, Request
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 from . import schemas, models
 from .database import SessionLocal, engine
@@ -21,9 +22,31 @@ def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
 
 
+def raise_not_found(request):
+    message = f"URL '{request.url}' doesn't exist."
+    raise HTTPException(status_code=404, detail=message)
+
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to URL Shortener!"}
+
+
+@app.get("/{url_key}")
+def forward_to_target_url(
+        url_key: str,
+        request: Request,
+        db: Session = Depends(get_db)
+):
+    db_url = (
+        db.query(models.URL)
+        .filter(models.URL.key == url_key, models.URL.is_active)
+        .first()
+    )
+    if db_url:
+        return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
 
 
 @app.post("/url", response_model=schemas.URLInfo)
